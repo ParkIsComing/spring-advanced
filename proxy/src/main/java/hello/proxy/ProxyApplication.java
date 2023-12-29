973d1fdf6f8f9767dea2ecb831e4f7a33ba59a07
Gayeong Park <maroony55@gmail.com>
2023-12-29 17:22:29 +0900

    7 : 스프링이 제공하는 빈 후처리기
    - 스프링 AOP 사용

--- proxy/src/main/java/hello/proxy/ProxyApplication.java
+++ proxy/src/main/java/hello/proxy/ProxyApplication.java
@@ -5,15 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
-import hello.proxy.config.AppV1Config;
-import hello.proxy.config.AppV2Config;
-import hello.proxy.config.v1_proxy.ConcreteProxyConfig;
-import hello.proxy.config.v1_proxy.InterfaceProxyConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
-import hello.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
-import hello.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
-import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
+import hello.proxy.config.v5_autoproxy.AutoProxyConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 
@@ -23,7 +15,8 @@ import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 // @Import(DynamicProxyFilterConfig.class)
 // @Import(ProxyFactoryConfigV1.class)
 // @Import(ProxyFactoryConfigV2.class)
-@Import(BeanPostProcessorConfig.class)
+// @Import(BeanPostProcessorConfig.class)
+@Import(AutoProxyConfig.class)
 // config 패키지에서 등록될 빈들은 @Import를 통해 따로 지정해 줄 것이기 때문에 app 패키지만 스캔하도록 하기 위해 scanBasePackages 설정 사용
 // 해당 설정을 생략하면 ProxyApplication이 있는 패키지와 그 하위 패키지를 스캔
 @SpringBootApplication(scanBasePackages = "hello.proxy.app")
